f3a2f2ee1992a03ad8f00ac7cc765d913e271ef2
Sagar <sagarkumar00008@gmail.com>
2025-10-21 22:07:50 +0530

    moveZeroes changes added

---
+++ array/moveZeroes.js
@@ -0,0 +1,23 @@
+// Input: [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+const nums1 = [0, 1, 0, 3, 12];
+
+const moveZeroes = (nums1) => {
+  console.log(nums1);
+  let lastNonZero = 0;
+  for (let i = 0; i < nums1.length; i++) {
+    console.log("status:", nums1[i]);
+    if (nums1[i] !== 0) {
+      const temp = nums1[lastNonZero];
+      console.log("temp:", temp);
+      nums1[lastNonZero] = nums1[i];
+      nums1[i] = temp;
+      lastNonZero++;
+    }
+  }
+  return nums1;
+};
+
+const result = moveZeroes(nums1);
+console.log("result====>", result);
